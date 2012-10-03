901e7097093f5a73516782fe052cfe8762dbfdf3
nkshigeru <nkshigeru@gmail.com>
2012-10-03 17:06:36 +0900

    improve value.hpp

--- include/mrbp/value.hpp
+++ include/mrbp/value.hpp
@@ -24,19 +24,48 @@
 #define MRBP_VALUE_H_
 
 #include <mruby.h>
+#include <mruby/numeric.h>
 
 namespace mrbp {
 
+//nil
 static inline mrb_value value()
 {
     return mrb_nil_value();
 }
 
-static inline mrb_value value(int v)
+//bool
+static inline mrb_value value(bool v)
+{
+    if (v)
+    {
+        return mrb_true_value();
+    }
+    else
+    {
+        return mrb_false_value();
+    }
+}
+
+//int
+static inline mrb_value value(mrb_int v)
 {
     return mrb_fixnum_value(v);
 }
 
+//float
+static inline mrb_value value(mrb_float v)
+{
+    return mrb_float_value(v);
+}
+
+//object
+static inline mrb_value value(void* v)
+{
+    return mrb_obj_value(v);
+}
+
+//mrb_value
 static inline mrb_value value(mrb_value v)
 {
     return v;
@@ -46,9 +75,78 @@ template<typename T>
 static inline bool check(mrb_value value);
 
 template<>
+static inline bool check<bool>(mrb_value value)
+{
+	return (mrb_type(value) == MRB_TT_TRUE) ||
+        (mrb_type(value) == MRB_TT_FALSE && mrb_fixnum(value));
+}
+
+template<>
+static inline bool check<mrb_int>(mrb_value value)
+{
+	return mrb_type(value) == MRB_TT_FIXNUM;
+}
+
+template<>
+static inline bool check<mrb_float>(mrb_value value)
+{
+	return mrb_type(value) == MRB_TT_FLOAT;
+}
+
+template<>
 static inline bool check<mrb_value>(mrb_value value)
 {
 	return true;
+}
+
+static inline void get(mrb_value value, bool& out)
+{
+	out = (mrb_type(value) != MRB_TT_FALSE);
+}
+
+static inline void get(mrb_value value, mrb_int& out)
+{
+    switch (mrb_type(value))
+    {
+    case MRB_TT_FIXNUM:
+	    out = mrb_fixnum(value);
+        break;
+    case MRB_TT_FLOAT:
+	    {
+            mrb_float f = mrb_float(value);
+
+            if (!FIXABLE(f)) {
+                throw std::range_error("float too big for int");
+            }
+            out = static_cast<mrb_int>(f);
+        }
+        break;
+    case MRB_TT_FALSE:
+        out = 0;
+        break;
+    default:
+        throw std::invalid_argument("can't convert to int");
+        break;
+    }
+}
+
+static inline void get(mrb_value value, mrb_float& out)
+{
+    switch (mrb_type(value))
+    {
+    case MRB_TT_FLOAT:
+        out = mrb_float(value);
+        break;
+    case MRB_TT_FIXNUM:
+	    out = static_cast<mrb_int>(mrb_fixnum(value));
+        break;
+    case MRB_TT_FALSE:
+        out = 0;
+        break;
+    default:
+        throw std::invalid_argument("can't convert to float");
+        break;
+    }
 }
 
 static inline void get(mrb_value value, mrb_value& out)
@@ -56,10 +154,7 @@ static inline void get(mrb_value value, mrb_value& out)
 	out = value;
 }
 
-static inline void get(mrb_value value, int& out)
-{
-	out = mrb_fixnum(value);
-}
+
 
 }
 
